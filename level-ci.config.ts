73e622de476f98a242771ac121707de5d27a584a
dmytro-fedkiv-la <dmytro.fedkiv@userway.org>
2025-07-25 15:37:14 -0400

    Add files via upload

---
+++ level-ci.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from '@userway/cicd-core';
+
+export default {
+  organization: 'dmytro-fedkiv',
+  project: 'level-ci-github-integration',
+  token: process.env.LEVEL_CI_TOKEN,
+  server: 'https://cicd.dev.userway.dev',
+  connection: 'https://cicd.dev.userway.dev',
+  pages: [
+    {
+      url: '/login?next=/dmytro-fedkiv/projects',
+      auth: {
+        url: 'https://cicd.dev.userway.dev/login',
+        type: 'form',
+        credentials: {
+          username: process.env.LEVEL_CI_USERNAME!,
+          password: process.env.LEVEL_CI_PASSWORD!,
+        },
+        selectors: {
+          username: 'form input[type="email"]',
+          password: 'form input[type="password"]',
+          submit: 'form button[type="submit"]',
+        },
+      },
+    },
+  ],
+} satisfies Config;
