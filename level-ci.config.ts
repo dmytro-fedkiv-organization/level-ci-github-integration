fc6bcc56c874446e34407387a414c18495302ceb
Dmytro Fedkiv <dmytro.fedkiv@levelaccess.com>
2025-09-08 13:33:18 -0400

    Refactor pipeline to use environment variable

--- level-ci.config.ts
+++ level-ci.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@userway/cicd-core";
 export default {
   organization: "dmytro-fedkiv",
   project: "level-ci-github-integration",
-  token: "h9cdjj0eiajp7zvyotfkxy2cgs6eyvchiukmzzgs",
+  token: process.env.LEVEL_CI_TOKEN!,
   reportPaths: ["./level-ci-reports"],
   server: "https://api.dev.userway.dev/",
 } satisfies Config;
